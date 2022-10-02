23f97349bef0d9b36cfb4bd6cc416ac2d527f57f
hav0x45 <pxksii@gmail.com>
2022-10-02 19:05:56 +0300

    Fixed texture not loading error by renaming old modid.

--- src/main/java/net/pekes/dowsing/DowsingMod.java
+++ src/main/java/net/pekes/dowsing/DowsingMod.java
@@ -9,7 +9,7 @@ public class DowsingMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final String MOD_ID = "dowsing";
+	public static final String MOD_ID = "pekesdowsing";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
